9c6473ca5df61ef257a0f96b5fadcbc477443e92
Nasik123 <74252620+Nasik123@users.noreply.github.com>
2020-11-10 20:11:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,375 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Owl Theme Default CSS -->
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<!-- Owl Carousel CSS -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<!-- Animate CSS -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- Boxicons CSS -->
+	<link rel="stylesheet" href="assets/css/boxicons.min.css">
+	<!-- Meanmenu CSS -->
+	<link rel="stylesheet" href="assets/css/meanmenu.css">
+	<!-- Magnific Popup CSS -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<!-- Nice Select CSS -->
+	<link rel="stylesheet" href="assets/css/nice-select.css">
+	<!-- Odometer CSS-->
+	<link rel="stylesheet" href="assets/css/odometer.css">
+	<!-- Corporate Style CSS -->
+	<link rel="stylesheet" href="assets/css/corporate-style.css">
+	<!-- Common CSS -->
+	<link rel="stylesheet" href="assets/css/common.css">
+	<!-- Corporate Responsive CSS -->
+	<link rel="stylesheet" href="assets/css/corporate-responsive.css">
+	<!-- Common Responsive CSS -->
+	<link rel="stylesheet" href="assets/css/common-responsive.css">
+	<link rel="stylesheet" href="assets/css/saas-style.css">
+	<link rel="stylesheet" href="assets/css/it-style.css">
+	<link rel="stylesheet" href="assets/css/it-responsive.css">
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="assets/img/favicon.png">
+	<!-- ICONS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- TITLE -->
+	<title>krishnaag IT solutions | Home</title>
+	<script>
+		var w = window;
+		var p = w.location.protocol;
+		if (p.indexOf("http") < 0) {
+			p = "http" + ":";
+		}
+		var d = document;
+		var f = d.getElementsByTagName('script')[0],
+			s = d.createElement('script');
+		s.type = 'text/javascript';
+		s.async = false;
+		if (s.readyState) {
+			s.onreadystatechange = function() {
+				if (s.readyState == "loaded" || s.readyState == "complete") {
+					s.onreadystatechange = null;
+					try {
+						loadwaprops('2573bc7da3e92eab5c2de2935621879ae', '29b4dc78a4518458829d179a3f27c66c4', '231767431606c2029b275818bc1c4041e1185630859ca1fd0', '27c3da7b0eadd7549e5d52eb2be66bf17', 0.0);
+					} catch (e) {}
+				}
+			};
+		} else {
+			s.onload = function() {
+				try {
+					loadwaprops('2573bc7da3e92eab5c2de2935621879ae', '29b4dc78a4518458829d179a3f27c66c4', '231767431606c2029b275818bc1c4041e1185630859ca1fd0', '27c3da7b0eadd7549e5d52eb2be66bf17', 0.0);
+				} catch (e) {}
+			};
+		};
+		s.src = p + '//mh.zoho.in/hub/js/WebsiteAutomation.js';
+		f.parentNode.insertBefore(s, f);
+	</script>
+	<style>
+		.pb-20{
+			height: 450px;
+		}
+		.h1,.h2,.h3,.h4,.h5,.h6,h1,h2,h3,h4,h5,h6 {
+          color: #1089d4;
+		}
+		</style>
+</head>
+
+<body>
+	<!-- Start Preloader Area -->
+	<div class="preloader">
+		<div class="lds-ripple">
+			<div></div>
+			<div></div>
+		</div>
+	</div>
+	<!-- End Preloader Area -->
+
+	<!-- Start Corporate Navbar Area -->
+	<?php include("includes/header.php"); ?>
+	<!-- End Corporate Navbar Area -->
+
+	<!-- Start Corporate Banner Area -->
+	<section class="corporate-banner-area jarallax" data-jarallax='{"speed": 0.3}'>
+		<div class="d-table">
+			<div class="d-table-cell">
+				<div class="container">
+					<div class="banner-text">
+						<div class="col-lg-12 col-md-12 order-lg-first">
+							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+								<ol class="carousel-indicators">
+									<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+								</ol>
+								<div class="carousel-inner pb-20">
+									<div class="carousel-item active">
+										<div class="row">
+											<div class="col-lg-6">
+												<div class="header-txt">
+													<h1 class="animated fadeInRight" data-animate="fadeInRight" data-delay=".80" style="visibility: visible; animation-delay: .80s;">Zoho Marketing Hub</h1>
+													<p class="lead animated fadeInRight" data-animate="fadeInRight" data-delay="1" style="visibility: visible; animation-delay: 1s;text-align:justify"> Zoho Marketing Hub - An all-in-One Marketing Automation tool which helps to monitor, manage & engage all your marketing operations across multiple channels. It also helps in generating & converting those leads to long term business opportunities. Our Consultants having deep domain knowledge and expertise provide you with end-to-end consulting & implementation support services globally which are designed to meet your current and future business goals.
+													</p>
+													<ul class="btns animated fadeInRight" data-animate="fadeInRight" data-delay="1.20" style="visibility: visible; animation-delay: 1.20s;">
+														<li>
+															<div class="wow fadeInLeft" data-wow-delay=".6s">
+																<a class="default-btn" href="contact.php">Find Out More</a>
+															</div>
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="col-lg-6">
+												<img src="home8.png" class="d-block w-100" alt="image">
+											</div>
+										</div>
+									</div>
+
+									<div class="carousel-item">
+										<div class="row">
+											<div class="col-lg-6">
+												<div class="header-txt">
+													<h1 class="animated fadeInRight" data-animate="fadeInRight" data-delay=".80" style="visibility: visible; animation-delay: .80s;">Lead-To-Cash CRM</h1>
+													<p class="lead animated fadeInRight" data-animate="fadeInRight" data-delay="1" style="visibility: visible; animation-delay: 1s;text-align:justify"> Lead-to-Cash - A CRM Solution which helps in increasing the productivity, streamlining the sales process and enhance the visibility for sales team within the organization. It gives a bird eye view which enhances the understanding of the lead. This approach helps in converting the lead to business and hence the name Lead-to-Cash. An improved engagement with the customer gives an opportunity to know your lead well and enhances the relationship.
+													</p>
+													<ul class="btns animated fadeInRight" data-animate="fadeInRight" data-delay="1.20" style="visibility: visible; animation-delay: 1.20s;">
+														<li>
+															<div class="wow fadeInLeft" data-wow-delay=".6s">
+																<a class="default-btn" href="contact.php">Find Out More</a>
+															</div>
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="col-lg-6">
+												<img src="home91.png" class="d-block w-100" alt="image">
+											</div>
+										</div>
+									</div>
+
+									<div class="carousel-item">
+										<div class="row">
+											<div class="col-lg-6">
+												<div class="header-txt">
+													<h1 class="animated fadeInRight" data-animate="fadeInRight" data-delay=".80" style="visibility: visible; animation-delay: .80s;">Stone ERP</h1>
+													<p class="lead animated fadeInRight" data-animate="fadeInRight" data-delay="1" style="visibility: visible; animation-delay: 1s;text-align:justify"> Stonework ERP is built to address all the unique challenges of the Granite Industry and its business processes. It helps in improving overall productivity by reducing the risk-taking factors within the company. Stonework ERP is designed and developed on Salesforce Platform which is niche and one of the Industry best players in customer relationship management. KNIT is proud to be the first company to provide the solution on Salesforce platform and serve the granite industry.This ERP solution is built to centralize business processes, improve productivity and thus reduce risk across the enterprise.
+													</p>
+													<ul class="btns animated fadeInRight" data-animate="fadeInRight" data-delay="1.20" style="visibility: visible; animation-delay: 1.20s;">
+														<li>
+															<div class="wow fadeInLeft" data-wow-delay=".6s">
+																<a class="default-btn" href="contact.php">Find Out More</a>
+															</div>
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="col-lg-6">
+												<img src="home13.png" class="d-block w-100" alt="image">
+											</div>
+										</div>
+									</div>
+
+									<div class="carousel-item">
+										<div class="row">
+											<div class="col-lg-6">
+												<div class="header-txt">
+													<h1 class="animated fadeInRight" data-animate="fadeInRight" data-delay=".80" style="visibility: visible; animation-delay: .80s;">Ecommerce Portal</h1>
+													<p class="lead animated fadeInRight" data-animate="fadeInRight" data-delay="1" style="visibility: visible; animation-delay: 1s;text-align:justify">With KNIT Sell more and promote your brand with state-of-the-art storefronts, optimized shopping experiences and an industry-leading suite of professional eCommerce apps.Offer customers secure checkout and handle business transactions seamlessly. Get competitive payment processing rates, custom payout schedules and dozens of payment providers.
+													</p>
+													<ul class="btns animated fadeInRight" data-animate="fadeInRight" data-delay="1.20" style="visibility: visible; animation-delay: 1.20s;">
+														<li>
+															<div class="wow fadeInLeft" data-wow-delay=".6s">
+																<a class="default-btn" href="contact.php">Find Out More</a>
+															</div>
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="col-lg-6">
+												<img src="home4.png" class="d-block w-100" alt="image">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	</section>
+	<!-- End Corporate Banner Area -->
+
+	<section class="corporate-business-solutions-area ptb-50" style="background-color:white;text-align:justify">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<div class="business-solutions-text">
+						<h2>Providing IT Solutions for more than 20 Years</h2>
+						<p> Krishnaag IT Solutions is a global IT services and consulting firm with a distinctive and eccentric approach that serves in accelerating business outcomes. In addition to that we provide solutions through digital enablement, seamless connectivity and data transformation.</p>
+						<p> We are partnered with top ecosystem of digital innovators, including MuleSoft, Salesforce, Microsoft Azure, HPE/MapR and Cloudera/Hortonworks, to develop leading business solutions that enable transformation and growth.</p>
+						<p> Our core competency lies in accuracy, efficiency & prompt deliverables to our esteemed clientele </p>
+						<!-- <ul style="list-style-type:disc;" class="pl-30"> -->
+						<ul>
+							<li> <i class='bx bx-check-square'></i> Accuracy - We will look closely at your current situation to ensure we chart the best path to deliver your desired outcome.</li>
+							<li> <i class='bx bx-check-square'></i> Efficiency - We specialise in change management and simplifying complexity, to help promote agility and responsiveness within your organisation.</li>
+							<li> <i class='bx bx-check-square'></i> Prompt - Our 300-strong team in the UK, US and India works around the clock to deliver future-proof solutions on time and on budget.</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="solutions-img wow fadeInRight" data-wow-delay=".2s">
+						<img src="assets/img/corporate-business/solutions.png" alt="Image">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="inner-it-what-we-do-area pt-50 pb-70" style="background-color:white;">
+		<div class="container">
+			<div class="section-title">
+				<h2>Why Krishnaag IT Solutions</h2>
+				<p> Since our inception in 1999, We have been profoundly dedicated to mastering our craft, staying at the cutting edge of system integrated technology. Delivering best outcome to our esteemed customers with transparency and insight. </p>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="title">
+						<h2>Our Approach</h2>
+					</div>
+					<p style="text-align: justify;">With par excellence our consultants competence in Mulesoft,Salesforce & Bigdata centres are pivotal in every success we achieve through our valued customers.</p>
+				</div>
+				<div class="col-md-4">
+					<div class="title">
+						<h2>Our People</h2>
+					</div>
+					<p style="text-align: justify;">Commitment is the synonym when it comes to our people.Our Team is eminent in delivering high standards to our customers.They are the ones who are with us at our every stepping stone towards success. By each day and our squad is growing strong.</p>
+				</div>
+				<div class="col-md-4">
+					<div class="title">
+						<h2>Our Clients </h2>
+					</div>
+					<p style="text-align: justify;">Our Clients success defines us and make what we are today.Our biggest achievement is the trust we gained by all big & small brands across wide range of industries. They count on us and we deliver their dreams. We are proud to share our successful journey with them </p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="inner-it-what-we-do-area pt-50 pb-70" style="background-color:white">
+		<div class="container">
+			<div class="section-title">
+				<h2>Featured Services</h2>
+				<p> Since our foundation in 1999, we have been dedicated to mastering our craft, staying at the cutting edge of system's integration technology and serving our customers with transparency and insight.</p>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<h2 style="color: black;">Etiam posuere augue</h2>
+					<div class="box">
+						<a href="#" class="image image-full"><img src="assets/img/bg.jpg" alt="image"></a>
+						<span>Maecenas luctus lectus</span>
+					</div>
+					<p>Vivamus fermentum augue. Praesent lacus urna rutrum. Nulla enim eros varius. Duis enim nulla luctus eu, dapibus lacinia.</p>
+					<a href="#" class="button">Etiam posuere</a>
+				</div>
+				<div class="col-md-4">
+					<h2 style="color: black;">Donec dictum metus</h2>
+					<div class="box">
+						<a href="#" class="image image-full"><img src="assets/img/bg.jpg" alt="image"></a>
+						<span>Lorem ipsum dolor sit amet</span>
+					</div>
+					<p>Vivamus fermentum augue. Praesent lacus urna rutrum. Nulla enim eros varius. Duis enim nulla luctus eu, dapibus lacinia.</p>
+					<a href="#" class="button">Etiam posuere</a>
+				</div>
+				<div class="col-md-4">
+					<h2 style="color: black;">Fusce ultrices</h2>
+					<div class="box">
+						<a href="#" class="image image-full"><img src="assets/img/bg.jpg" alt="image"></a>
+						<span>Pellentesque quis lectus</span>
+					</div>
+					<p>Vivamus fermentum augue. Praesent lacus urna rutrum. Nulla enim eros varius. Duis enim nulla luctus eu, dapibus lacinia.</p>
+					<a href="#" class="button">Etiam posuere</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<?php include("includes/footer.php"); ?>
+
+	<!-- Start Go Top Area -->
+	<div class="go-top">
+		<i class='bx bx-caret-up'></i>
+		<i class='bx bx-caret-up'></i>
+	</div>
+	<!-- End Go Top Area -->
+
+	<script src="assets/js/platform.js" defer></script>
+
+	<!-- Jquery Slim JS -->
+	<script src="assets/js/jquery.slim.min.js"></script>
+	<!-- Popper JS -->
+	<script src="assets/js/popper.min.js"></script>
+	<!-- Bootstrap JS -->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- Meanmenu JS -->
+	<script src="assets/js/jquery.meanmenu.js"></script>
+	<!-- Wow JS -->
+	<script src="assets/js/wow.min.js"></script>
+	<!-- Owl Carousel JS -->
+	<script src="assets/js/owl.carousel.js"></script>
+	<!-- Mixitup JS -->
+	<script src="assets/js/jquery.mixitup.min.js"></script>
+	<!-- Nice Select JS -->
+	<script src="assets/js/jquery.nice-select.min.js"></script>
+	<!-- Magnific-Popup JS -->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- Appear JS -->
+	<script src="assets/js/jquery.appear.js"></script>
+	<!-- Odometer JS -->
+	<script src="assets/js/odometer.min.js"></script>
+	<!-- Parallax JS -->
+	<script src="assets/js/parallax.min.js"></script>
+	<!-- Map JS FILE -->
+	<script src="assets/js/map.js"></script>
+	<!-- Custom JS -->
+	<script src="assets/js/custom.js"></script>
+	<script>
+		var w = window;
+		var p = w.location.protocol;
+		if (p.indexOf("http") < 0) {
+			p = "http" + ":";
+		}
+		var d = document;
+		var f = d.getElementsByTagName('script')[0],
+			s = d.createElement('script');
+		s.type = 'text/javascript';
+		s.async = false;
+		if (s.readyState) {
+			s.onreadystatechange = function() {
+				if (s.readyState == "loaded" || s.readyState == "complete") {
+					s.onreadystatechange = null;
+					try {
+						loadwaprops("27218d28c96aa859ebdab7f8fc3213c55", "2ff43d5e6658a1bf5d71b62c3142be692", "2c79c2c690458773fe5c809b84b62707b72f384f8d12b7f6c", "2bcd5176fc905b964d913bb10db670889c45137be9522690f", "0.0");
+					} catch (e) {}
+				}
+			};
+		} else {
+			s.onload = function() {
+				try {
+					loadwaprops("27218d28c96aa859ebdab7f8fc3213c55", "2ff43d5e6658a1bf5d71b62c3142be692", "2c79c2c690458773fe5c809b84b62707b72f384f8d12b7f6c", "2bcd5176fc905b964d913bb10db670889c45137be9522690f", "0.0");
+				} catch (e) {}
+			};
+		};
+		s.src = p + "//mh.zoho.com/hub/js/WebsiteAutomation.js";
+		f.parentNode.insertBefore(s, f);
+	</script>
+</body>
+
+</html>
